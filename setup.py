b6ddb2c5c8aaa5d082bdfb69a85f5ce8f6886c6c
Pranav Tondgaonkar <pranavrelds@gmail.com>
2023-06-17 02:14:50 +0200

    commit without UI and workflow

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="price_prediction",
+    version="0.0.1",
+    author="Pranav Tondgaonkar",
+    author_email="pranavrelds@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
